f09b774d857f0e50b616d5477d21d94332970e93
liling <liling39@meituan.com>
2020-06-01 13:03:33 +0800

    链表节点

--- ListNode.java
+++ ListNode.java
@@ -1,4 +1,20 @@
 package leetcode;
 
+
+//  Definition for singly-linked list.
 public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
