c86acaab653e93e978006d6e79f113f81511a277
liling <liling39@meituan.com>
2020-06-03 13:48:37 +0800

    83.删除排序链表中的重复元素

---
+++ DeleteDuplicates.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->1->2
+ * 输出: 1->2
+ * 示例 2:
+ *
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ *
+ *
+ */
+public class DeleteDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null){
+            if(current.val == current.next.val){
+                current.next = current.next.next;
+            }else{
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
