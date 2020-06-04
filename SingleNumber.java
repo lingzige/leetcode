f26292f75301164e9d2872bd7072c0ddb3944052
liling <liling39@meituan.com>
2020-06-04 13:31:16 +0800

    136.只出现一次的数字

---
+++ SingleNumber.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] arr = {2,2,1};
+        System.out.println(singleNumber(arr));
+    }
+    public static int singleNumber(int[] nums) {
+        int s = nums[0];
+        for(int i=1;i<nums.length;i++){
+            s = s^nums[i];
+        }
+        return s;
+    }
+}
