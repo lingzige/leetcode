f0f346feb191c60bcc7d5756617149e6f56d8e25
liling <liling39@meituan.com>
2020-06-09 13:34:06 +0800

    46.把数字翻译成字符串

---
+++ TranslateNum.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成
+ * “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: 12258
+ * 输出: 5
+ *解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+ *
+ */
+public class TranslateNum {
+    public int translateNum(int num) {
+        String src = String.valueOf(num);
+        // r=1表示每个字符单独算一位，一定有一种组合
+        int p = 0, q = 0, r = 1;
+        for (int i = 0; i < src.length(); ++i) {
+            p = q;
+            q = r;
+            r = 0;
+            r += q;
+            if (i == 0) {
+                continue;
+            }
+            String pre = src.substring(i - 1, i + 1);
+            if (pre.compareTo("25") <= 0 && pre.compareTo("10") >= 0) {
+                r += p;
+            }
+        }
+        return r;
+    }
+}
